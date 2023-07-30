90573e6c4ae5339a96da835a3ccaae283bb9021d
dustin <58792893+dshook420@users.noreply.github.com>
2023-07-30 00:02:44 -0400

    init sidebar menu

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// DOM LOADED
+
+document.addEventListener('DOMContentLoaded', () => {
+    const sidebar = document.getElementById('sidebar');
+    const header = document.getElementById('header');
+    const main = document.getElementById('main');
+
+    // SIDEBAR
+    renderSidebar(sidebar);
+
+});
