7b57b4ad0b191ce63b66c7e246a7d9e04a8831de
dustin <58792893+dshook420@users.noreply.github.com>
2023-08-04 18:01:08 -0400

    side bar js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,31 @@
 // DOM LOADED
 
 document.addEventListener('DOMContentLoaded', () => {
+    const wrapper = document.getElementById('layout');
     const sidebar = document.getElementById('sidebar');
-    const header = document.getElementById('header');
-    const main = document.getElementById('main');
 
-    // SIDEBAR
-    /* renderSidebar(sidebar); */
+    const sidebarToggle = document.getElementById('expand');
+    const icon = sidebarToggle.querySelector('i');
+    const menuText = sidebar.querySelectorAll('.menu-text');
+
+    sidebarToggle.addEventListener('click', () => {
+        
+        sidebar.classList.toggle('active-sidebar');
+        wrapper.classList.toggle('active-sidebar');
+
+        menuText.forEach((item) => {
+            item.classList.toggle('hidden');
+        });
+
+        if (icon.classList.contains('fa-bars')) {
+            icon.classList.remove('fa-bars');
+            icon.classList.add('fa-bars-staggered');
+        } else {
+            icon.classList.remove('fa-bars-staggered');
+            icon.classList.add('fa-bars');
+        }
+
+
+    });
 
 });
